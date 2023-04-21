be02a504d24bff49323715c2b573463bdc6cb28f
xiaoxiao yan <76906208+yanxiao333@users.noreply.github.com>
2023-04-21 12:43:56 +0800

    1

--- spider_test_4_4.py
+++ spider_test_4_4.py
@@ -2,6 +2,7 @@
 
 from requests_html import HTMLSession
 from copyheaders import headers_raw_to_dict
+from gne import GeneralNewsExtractor
 
 url = 'https://www.msn.cn/zh-cn/news/redstarnews?ocid=BHEA000&cvid=f14e433625d645f8a49c3143b89aab09&ei=5'
 str = b'''
@@ -9,6 +10,7 @@ User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 '''
 session = HTMLSession()
 headers = headers_raw_to_dict(str)
+extractor = GeneralNewsExtractor()
 
 
 def main():
@@ -21,8 +23,27 @@ def main():
         for data1 in details:
             detail = data1.attrs['aria-label']
             detail_url = 'https://www.msn.cn'+data1.attrs['href']
-            print(detail, detail_url)
+            # print(detail, detail_url)
+        return detail_url
+    else:
+        return None
+
+
+def get_download_content(detail_url):
+    try:
+        res1 = session.get(url=detail_url, headers=headers)
+        print(res1.status_code, res1.encoding)
+        if res1.status_code == 200:
+            html = res1.text
+            print(html)
+            exit()
+            content = extractor.extract(html)
+            print(content)
+        pass
+    except Exception as e:
+        print('Error downloading', e)
 
 
 if __name__ == '__main__':
-    main()
+    detail_url = main()
+    get_download_content(detail_url)
