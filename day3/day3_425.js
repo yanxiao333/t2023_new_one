d6fe5e8f5f2cc54aeb06fcecb3b04f1759ce04d7
yanxiao333 <yanxiao_333@126.com>
2023-04-27 19:11:55 +0800

    4_27

---
+++ day3/day3_425.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: yanxiao333 yanxiao_333@126.com
+ * @Date: 2023-04-25 20:48:11
+ * @LastEditors: yanxiao333 yanxiao_333@126.com
+ * @LastEditTime: 2023-04-25 20:48:48
+ * @FilePath: \t2023_new_one\day3\day3_425.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+
+
