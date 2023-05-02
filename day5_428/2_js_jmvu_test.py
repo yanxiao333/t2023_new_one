358c65f1ae825ba4efb091f0f59ee2d8441ce543
yanxiao333 <yanxiao_333@126.com>
2023-05-02 15:56:02 +0800

    5_2set.json

--- day5_428/2_js_jmvu_test.py
+++ day5_428/2_js_jmvu_test.py
@@ -2,7 +2,7 @@
 Author: yanxiao333 yanxiao_333@126.com
 Date: 2023-04-29 20:01:43
 LastEditors: yanxiao333 yanxiao_333@126.com
-LastEditTime: 2023-04-30 21:01:47
+LastEditTime: 2023-05-02 15:54:47
 FilePath: \t2023_new_one\day5_428\2_js_jmvu_test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
--- day5_428/3_js_七麦数据.py
+++ day5_428/3_js_七麦数据.py
@@ -2,7 +2,7 @@
 Author: yanxiao333 yanxiao_333@126.com
 Date: 2023-04-30 20:04:53
 LastEditors: yanxiao333 yanxiao_333@126.com
-LastEditTime: 2023-05-01 20:25:42
+LastEditTime: 2023-05-02 15:17:08
 FilePath: \t2023_new_one\day5_428\3_js_七麦数据.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
